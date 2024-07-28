14651db63eb83e7a30431700b0bf7d33487d77f6
JasonWei <9979023+timesons@users.noreply.github.com>
2024-07-28 15:22:10 +0800

    Update env.js

--- env.js
+++ env.js
@@ -2,13 +2,13 @@
 window.routerBase = 'https://new.gjtq.net/'
 window.settings = {
   // 站点名称
-  title: 'Airport404',
+  title: 'Airport404隐形机场',
   // 站点描述
   description: 'www.airport404.com',
   assets_path: '/assets',
   // 主题色
   theme: {
-    color: 'default', //可选 default、blue、black、darkblue
+    color: 'black', //可选 default、blue、black、darkblue
   },
   // 版本号
   version: '0.1.1-dev',
