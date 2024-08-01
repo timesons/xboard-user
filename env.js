0fdc45670c4d67bd72c70300de1e9b8d02f15fd5
JasonWei <9979023+timesons@users.noreply.github.com>
2024-08-02 02:19:00 +0800

    Update env.js

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
 window.routerBase = 'https://gjtq.net/'
 window.settings = {
   // 站点名称
-  title: (window.location.host.search('ysjc') != -1) ? "隐身机场" : ((window.location.host.search('tizi001') != -1) ? "梯子001" : ((window.location.host.search('tizi100') != -1)) ? "梯子100" : ((window.location.host.search('fq100') != -1)) ? "翻墙100" : ((window.location.host.search('jiasu001') != -1)) ? "加速001" : ((window.location.host.search('jiasu100') != -1)) ? "加速100" : ((window.location.host.search('kxsw100') != -1)) ? "科学上网100" :  ((window.location.host.search('hidemysel') != -1)) ? 'HideMySelf' : "狗急跳墙"),
+  title: (window.location.host.search('ysjc') != -1) ? "隐身机场" : ((window.location.host.search('gjtq') != -1) ? "狗急跳墙" : ((window.location.host.search('tizi100') != -1)) ? "梯子100" : ((window.location.host.search('fq100') != -1)) ? "翻墙100" : ((window.location.host.search('jiasu001') != -1)) ? "加速001" : ((window.location.host.search('jiasu100') != -1)) ? "加速100" : ((window.location.host.search('kxsw100') != -1)) ? "科学上网100" :  ((window.location.host.search('hidemyself') != -1)) ? "HideMySelf" : ((window.location.host.search('hidemytail') != -1)) ? "HideMyTail" : ((window.location.host.search('hidemyass') != -1)) ? "HideMyAss" : "XXX"),
   // 站点描述
   description: 'https://' + window.location.host,
   assets_path: '/assets',
