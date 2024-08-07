d7aab877f28a7bc3f57ae1dd042a87c849c3be61
JasonWei <9979023+timesons@users.noreply.github.com>
2024-08-07 23:10:10 +0800

    a
    a

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
 window.routerBase = 'https://gjtq.net/'
 window.settings = {
   // 站点名称
-  title: (window.location.host.search('dogvpn') != -1) ? "dogVPN" : ((window.location.host.search('gjtq') != -1) ? "狗急跳墙" : ((window.location.host.search('dogssl') != -1)) ? "dogSSL" : ((window.location.host.search('hidemyself') != -1)) ? "HideMySelf" : ((window.location.host.search('hidemytail') != -1)) ? "HideMyTail" : "XXX"),
+  title: (window.location.host.search('dogvpn') != -1) ? "dogVPN" : (window.location.host.search('dogrun') != -1) ? "dodRun" : ((window.location.host.search('gjtq') != -1) ? "狗急跳墙" : ((window.location.host.search('dogssl') != -1)) ? "dogSSL" : ((window.location.host.search('hidemyself') != -1)) ? "HideMySelf" : ((window.location.host.search('hidemytail') != -1)) ? "HideMyTail" : "XXX"),
   // 站点描述
   description:  (window.location.host.search('gjtq') != -1) ? '狗急跳墙(oﾟ▽ﾟ)o欢呼~-gjtq' : 'https://' + window.location.host,
   assets_path: '/assets',
