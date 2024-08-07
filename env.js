56ae01b64898bf04bad2c3c12aa60a57db8e8bd2
JasonWei <9979023+timesons@users.noreply.github.com>
2024-08-07 23:20:07 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,10 +1,10 @@
 // API地址
-window.routerBase = 'https://gjtq.net/'
+window.routerBase = 'https://www.gjtq.net/'
 window.settings = {
   // 站点名称
-  title: (window.location.host.search('dogvpn') != -1) ? "dogVPN" : (window.location.host.search('dogrun') != -1) ? "dodRun" : ((window.location.host.search('gjtq') != -1) ? "狗急跳墙" : ((window.location.host.search('dogssl') != -1)) ? "dogSSL" : ((window.location.host.search('hidemyself') != -1)) ? "HideMySelf" : ((window.location.host.search('hidemytail') != -1)) ? "HideMyTail" : "XXX"),
+  title: (window.location.host.search('dogvpn') != -1) ? "小狗VPN" : (window.location.host.search('dogrun') != -1) ? "小狗快跑" : ((window.location.host.search('gjtq') != -1) ? "狗急跳墙" : ((window.location.host.search('dogssl') != -1)) ? "小狗加速": ((window.location.host.search('hidemytail') != -1)) ? "HideMyTail" : "XXX"),
   // 站点描述
-  description:  (window.location.host.search('gjtq') != -1) ? '狗急跳墙(oﾟ▽ﾟ)o欢呼~-gjtq' : 'https://' + window.location.host,
+  description:  (window.location.host.search('gjtq') != -1) ? '狗急跳墙(oﾟ▽ﾟ)o欢呼~-GJTQ!' : (window.location.host.search('dogrun') != -1) ? '小狗快跑(oﾟ▽ﾟ)o欢呼~-dogRun!' : (window.location.host.search('dogssl') != -1) ? '小狗加速(oﾟ▽ﾟ)o欢呼~-dogSSL!'  : (window.location.host.search('dogvpn') != -1) ? '小狗VPN(oﾟ▽ﾟ)o欢呼~-dogVPN!' : 'https://' + window.location.host,
   assets_path: '/assets',
   // 主题色
   theme: {
