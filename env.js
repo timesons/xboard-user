fc778d56a6f4134f5be377f225ad01c6eb5236ea
JasonWei <9979023+timesons@users.noreply.github.com>
2024-09-02 01:39:34 +0800

    a
    a

--- env.js
+++ env.js
@@ -10,9 +10,9 @@ const isHidemytail = window.location.host.includes('hidemytail');
 
 window.settings = {
   // 站点名称
-  title: isDogvpn ? "小狗VPN" : isDogrun ? "小狗快跑dogRun" : isGjtq ? "狗急跳墙GJTQ" : isDogssl ? "小狗加速dogSSL" : isHidemytail ? "HideMyTail" : "默认名称",
+  title: isDogvpn ? "小狗VPN" : isDogrun ? "小狗快跑DogRun" : isGjtq ? "狗急跳墙GJTQ" : isDogssl ? "小狗加速DogSSL" : isHidemytail ? "HideMyTail" : "默认名称",
   // 站点描述
-  description: isGjtq ? '狗急跳墙(oﾟ▽ﾟ)o欢呼~-GJTQ!' : isDogrun ? '小狗快跑(oﾟ▽ﾟ)o欢呼~-dogRun!' : isDogssl ? '小狗加速(oﾟ▽ﾟ)o欢呼~-dogSSL!' : isDogvpn ? '小狗VPN(oﾟ▽ﾟ)o欢呼~-dogVPN!' : isHidemytail ? 'HideMyTail::Unblock Websites with HMT' : '默认描述',
+  description: isGjtq ? '狗急跳墙(oﾟ▽ﾟ)o欢呼~-GJTQ!' : isDogrun ? '小狗快跑(oﾟ▽ﾟ)o欢呼~-DogRun!' : isDogssl ? '小狗加速(oﾟ▽ﾟ)o欢呼~-DogSSL!' : isDogvpn ? '小狗VPN(oﾟ▽ﾟ)o欢呼~-DogVPN!' : isHidemytail ? 'HideMyTail::Unblock Websites with HMT' : '默认描述',
   assets_path: '/assets',
   // 主题色
   theme: {
