40275e711944378f19a0fca801c2ecab52cc13a7
JasonWei <9979023+timesons@users.noreply.github.com>
2024-09-12 19:09:28 +0800

    Update env.js

--- env.js
+++ env.js
@@ -21,9 +21,9 @@ window.settings = {
   // 版本号
   version: '0.1.1-dev',
   // 登陆背景
-  background_url: 'https://user.gjtq.net/world-vertical-circle-light.svg',
+  background_url: 'https://pub-7b2c70e035c9410197966e5e06a22b02.r2.dev/world-vertical-circle-light.svg',
   // 站点LOGO
-  logo: 'https://user.gjtq.net/dog-logo.svg',
-  favicon: 'https://user.gjtq.net/favicon.ico',
+  logo: 'https://pub-7b2c70e035c9410197966e5e06a22b02.r2.dev/dog-logo.svg',
+  favicon: 'https://pub-7b2c70e035c9410197966e5e06a22b02.r2.dev/favicon.ico',
   copyright:'Copyright 2017-2024 All Rights Reserved'
 };
